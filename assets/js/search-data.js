6276480b5990808d59980fa1503aaca625c787cf
Ellimac0 <154609534+Ellimac0@users.noreply.github.com>
2026-05-18 13:34:59 +0000

    Deploying to gh-pages from @ Ellimac0/Ellimac0.github.io@414d6494f5d2ae7c31af594ccc00a3458ccb92bd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,22 +412,7 @@ ninja.data = [{
           section: "News",},{id: "news-i-m-on-the-postdoc-job-market-looking-for-positions-on-neural-scene-representations-large-scale-3d-reconstruction-or-the-vision-geometry-remote-sensing-interface-get-in-touch-if-my-profile-fits-email",
           title: 'I’m on the postdoc job market! Looking for positions on neural scene representations,...',
           description: "",
-          section: "News",},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
+          section: "News",},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
