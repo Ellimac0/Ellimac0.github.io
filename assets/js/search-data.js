ac22715bdeb1468ac47ddd38a1e1368e1fa0c4f2
Ellimac0 <154609534+Ellimac0@users.noreply.github.com>
2026-05-18 13:38:30 +0000

    Deploying to gh-pages from @ Ellimac0/Ellimac0.github.io@9c48ec312802a88f454379c2a7b25108e9c01dce 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "My research code is open-source. Feel free to use it, cite it, or open an issue.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
